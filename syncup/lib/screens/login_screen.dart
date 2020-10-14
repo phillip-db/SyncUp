8b6d7e2979a5e7dd7a62816a448fe56d39b195e4
Phillip Burleigh <pburle2@illinois.edu>
2020-10-14 00:52:38 -0500

    Updated login screen

--- syncup/lib/screens/login_screen.dart
+++ syncup/lib/screens/login_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import '../main.dart';
 import 'main_screen.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -10,17 +9,63 @@ class LoginScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text('Login Screen'),
+        title: Text('Login'),
       ),
       body: Container(
-        decoration: opacityBackground(),
-        child: Center(
-          child: RaisedButton(
-            child: Text('Launch screen'),
-            onPressed: () {
-              Navigator.pushNamed(context, MainScreen.route);
-            },
-          ),
+        width: double.infinity,
+        color: Colors.grey[800],
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 80),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image(
+                    width: MediaQuery.of(context).size.width * 0.2,
+                    image: AssetImage('assets/images/song_placeholder.png'),
+                  ),
+                  Text(
+                    'SyncUp',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                    ),
+                    textScaleFactor: 4.5,
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.1,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 16.0),
+              child: GestureDetector(
+                onTap: () => Navigator.pushNamed(context, MainScreen.route),
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.4,
+                  child: Image(
+                    image: AssetImage('assets/images/spotify_logo.png'),
+                  ),
+                ),
+              ),
+            ),
+            Text(
+              'Log in with Spotify',
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.13,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.5,
+              child: Text(
+                'By logging in, you agree to the SyncUp Terms of Service',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 12),
+              ),
+            ),
+          ],
         ),
       ),
     );
